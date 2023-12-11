3ef24cb390e4d0198cacd121d0a896a194aa8eb8
rasmi <rasminoel@gmail.com>
2023-12-11 06:22:31 +0300

    Done

--- lib/student.py
+++ lib/student.py
@@ -4,5 +4,16 @@ from user import User
 
 class Student(User):
     
+    knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+    
     def learn(self):
-        pass
+        self.knowledge += random.randint(0, 10)
